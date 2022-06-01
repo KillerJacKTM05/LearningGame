ce64550328cbafdea939b473856ddb8e95fecee2
KillerJacKTM05 <dogukankvrak@hotmail.com>
2022-06-02 01:05:35 +0300

    In game settings
    settings are now appliable and saveable. You can save your own settings and continue to play.

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -12,11 +12,21 @@ public class MainMenu : MonoBehaviour
     public GameObject lobbyCanvas;
     public GameObject serverCanvas;
     public Slider volumeSlider;
+    public Dropdown qualityDropdown;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
     void Start()
     {
+        resolutions = Screen.resolutions;
         settingsCanvas.SetActive(false);
         lobbyCanvas.SetActive(false);
         volumeSlider.value = SoundManager.instance.GetSource().volume;
+        GetResolutions();
+        GetQuality();
+        resolutionDropdown.RefreshShownValue();
+        qualityDropdown.RefreshShownValue();
+        LoadSettings();
     }
 
     // Update is called once per frame
@@ -73,5 +83,54 @@ public class MainMenu : MonoBehaviour
     public void changeVolume(float sliderValue)
     {
         SoundManager.instance.GetSource().volume = sliderValue;
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void GetResolutions()
+    {
+        List<string> options = new List<string>();
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            //Debug.Log("option:" + option);
+            options.Add(option);
+        }
+        resolutionDropdown.AddOptions(options);
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        resolutionDropdown.value = resolutionIndex;
+    }
+    public void GetQuality()
+    {
+        List<string> options = new List<string>();
+        for(int i = 0; i < QualitySettings.names.Length; i++)
+        {
+            options.Add(QualitySettings.names[i]);
+        }
+        qualityDropdown.AddOptions(options);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        qualityDropdown.value = qualityIndex;
+    }
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value);
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+        PlayerPrefs.SetFloat("VolumePreference", volumeSlider.value);
+        PlayerPrefs.SetInt("FullscreenPreference", System.Convert.ToInt32(Screen.fullScreen));
+    }
+    public void LoadSettings()
+    { 
+        qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
+        resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
+        Screen.fullScreen = System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
     }
 }
